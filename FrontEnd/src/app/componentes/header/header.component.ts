1fd22338580c9304a21018a1cea43751bba99d83
emilio <eljishiemilio@gmail.com>
2025-04-27 16:41:13 +0200

    añadido boton en header

--- FrontEnd/src/app/componentes/header/header.component.ts
+++ FrontEnd/src/app/componentes/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonBackButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton]
+  imports: [IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonBackButton]
 })
 export class HeaderComponent implements OnInit {
   @Input() pageTitle: string = '';
