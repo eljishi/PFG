10593c34587f09fc982fc447b43da439a0769270
emilio <eljishiemilio@gmail.com>
2025-04-28 00:44:23 +0200

    Arregalo que no cargue pagina de vidos pero no llegando informacion desde la bd

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MenuLateralComponent } from './componentes/menu-lateral/menu-lateral.component';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
--- FrontEnd/src/app/common/ejercicios.ts
+++ FrontEnd/src/app/common/ejercicios.ts
@@ -1,12 +1,32 @@
 export interface Ejercicios {
-    _id: string
-    name: string
-    video: string
-    description: string
-    nullCauses: NullCause[]
-  }
+  id: number;
+  name: string;
+  video: string;
+  description: string;
+  nullCauses: NullCause[]
+}
+
+export interface NullCause {
+  reason: string
+  image: string
+}
+
+export interface ApiResponseEjercicio {
+  status: string;
+  data: Ejercicios;
+}
+export interface ApiResponseEjercicios {
+  status: string;
+  data: Ejercicios[];
+}
+export interface ApiResponseNullCause {
+  status: string;
+  data: NullCause[];
+}
+export interface ApiResponseMessage {
+  status: string;
+  message: string;
+}
   
-  export interface NullCause {
-    reason: string
-    image: string
-  }
+  
+  
