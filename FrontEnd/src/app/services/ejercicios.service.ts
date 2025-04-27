ae9bcda2d0982ad70b48a19ebf4fc864ae1997bd
emilio <eljishiemilio@gmail.com>
2025-04-27 19:03:11 +0200

    serviciios creados environment bien puesto y creadas funcion de ejercicios

---
+++ FrontEnd/src/app/services/ejercicios.service.ts
@@ -0,0 +1,22 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Ejercicios} from "../common/ejercicios";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EjerciciosService {
+  private readonly httpClient: HttpClient = inject(HttpClient)
+
+  getEjercicios(): Observable<Ejercicios[]> {
+    return this.httpClient.get<Ejercicios[]>(`${environment.urlBase}ejercicios`);
+  }
+
+  getEjercicio(id: string): Observable<Ejercicios> {
+    return this.httpClient.get<Ejercicios>(`${environment.urlBase}ejercicio/${id}`);
+  }
+
+  constructor() { }
+}
